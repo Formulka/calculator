f107d1cf980c915b398de06ef957179bd35aca6d
Petr Olah <petr.olah@outcomm.cz>
2015-05-07 10:49:58 +0200

    more parse_line test cases

--- tests.py
+++ tests.py
@@ -8,7 +8,20 @@ from calc import extract_instructions, parse_line
 class CalcTests(unittest.TestCase):
 
     def test_parse_line(self):
+        # test OK case
         self.assertEquals(parse_line("add 5"), ("add", 5))
+
+        # test case with whitespaces
+        self.assertEquals(parse_line("subtract 4\n"), ("subtract", 4))
+
+        # test case with invalid value
+        with self.assertRaises(ValueError):
+            parse_line("add foo")
+
+        # test case with invalid format
+        with self.assertRaises(ValueError):
+            parse_line("add foo bar")
+
 
     def test_extract_instructions(self):
         source = """
@@ -21,7 +34,10 @@ class CalcTests(unittest.TestCase):
         source_iterable = source.split('\n')
         result = [("add", 5), ("subtract", 2), ("divide", 5), ("add", 3), ("apply", 5)]
 
+        # test OK case
         self.assertEquals(extract_instructions(source_iterable), result)
+
+
 
 def main():
     unittest.main()
